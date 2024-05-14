9698761c8a714f35ad90c43894523863338ab7c5
Entony Maxwell <entonymaxwell01@gmail.com>
2024-05-14 19:42:38 -0300

    Adicionando elementos ao page de catalogos

--- cypress/pages/catalog/elements.js
+++ cypress/pages/catalog/elements.js
@@ -2,7 +2,9 @@ export const elements = {
   product: ".post-2559 > .product-block",
   sizeProduct: ".button-variable-item-S",
   colorProduct: ".button-variable-item-Blue",
+  quantityProduct: ".input-text",
   addCartButton: ".single_add_to_cart_button",
   textAddProduct: ".woocommerce-message",
   toGoCart: ".woocommerce-message > .button",
+  cartQuantity: ".quantity > .input-text",
 };
