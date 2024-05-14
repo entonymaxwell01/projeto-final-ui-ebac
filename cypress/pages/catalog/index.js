29c76704747b494f0f65bb68d41cec30aa2e8ecc
Entony Maxwell <entonymaxwell01@gmail.com>
2024-05-14 19:43:30 -0300

    Adicionando metodo de adicionar produto

--- cypress/pages/catalog/index.js
+++ cypress/pages/catalog/index.js
@@ -6,18 +6,20 @@ class Catalog {
     cy.url().should("eq", "http://lojaebac.ebaconline.art.br/produtos/");
   }
 
-  addProduct() {
+  addProduct(quantity) {
     cy.get(elements.product).click();
     cy.get(elements.sizeProduct).click();
     cy.get(elements.colorProduct).click();
+    cy.get(elements.quantityProduct).click().clear().type(quantity);
     cy.get(elements.addCartButton).click();
 
-    cy.get(elements.textAddProduct).should(
-      "contain.text",
-      "foi adicionado no seu carrinho"
-    );
+    cy.get(elements.textAddProduct).should("contain.text", "no seu carrinho");
     cy.get(elements.toGoCart).click();
     cy.screenshot("Adicionando item ao carrinho");
+  }
+
+  errorProductQuantity() {
+    this.addProduct(11);
   }
 }
 
