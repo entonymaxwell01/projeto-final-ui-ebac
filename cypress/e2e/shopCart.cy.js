bc1c9fdd423d9b1483f7530681762c6d072701f4
Entony Maxwell <entonymaxwell01@gmail.com>
2024-05-14 19:41:01 -0300

    Realizando testes do modulo de carrinho de compras

--- cypress/e2e/shopCart.cy.js
+++ cypress/e2e/shopCart.cy.js
@@ -9,6 +9,8 @@
 
 import Login from "../pages/login";
 import Catalog from "../pages/catalog";
+import Cart from "../pages/cart";
+import Checkout from "../pages/checkout";
 
 describe("Adicionando itens ao carrinho", () => {
   beforeEach(() => {
@@ -17,7 +19,17 @@ describe("Adicionando itens ao carrinho", () => {
     Catalog.visitPage();
   });
 
-  it.only("Deve adicionar um item ao carrinho", () => {
+  it("Deve adicionar um item ao carrinho", () => {
     Catalog.addProduct();
+  });
+  it("Deve confirmar ordem de pedido", () => {
+    Catalog.addProduct();
+    Cart.confirmPage();
+    Checkout.confirmPage();
+    Checkout.confirmOrder();
+  });
+
+  it.only("Deve apresentar erro ao adicionar mais de 10 itens", () => {
+    Catalog.errorProductQuantity();
   });
 });
